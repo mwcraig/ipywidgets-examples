6d04b35ddd093abd5550e35cf76e884ac2a15657
Matthew Craig <mattwcraig@gmail.com>
2017-06-20 12:00:55 -0500

    Adjustments for ipywidgets 7

--- gui.py
+++ gui.py
@@ -6,25 +6,27 @@ from traitlets import link
 from model import PassGen, SPECIAL_GROUPS
 
 
-class PassGenGUI(Box):
+class PassGenGUI(VBox):
     """
-    Class to encapsulate the interface for a simply polynomial
-    graphing tool.
-
+    Class to encapsulate the interface for a simple password generator.
     """
     def __init__(self):
-        super(PassGenGUI, self).__init__(description='Graph container')
+        super(PassGenGUI, self).__init__(description='Password container')
         self.model = PassGen()
         self.layout.width = '100%'
-        self._length = IntSlider(min=8, max=20, description='Password Length')
+        self.layout.align_items = 'flex-start'
+        style = {'description_width': 'initial'}
+        self._length = IntSlider(min=8, max=20,
+                                 description='Password Length',
+                                 style=style)
         link((self._length, 'value'), (self.model, 'password_length'))
+
+        self._top_label = Label(value='Generated password:',
+                                style=style)
 
         self._special = self._special_char_box()
 
         self._numbers = self._number_box()
-
-
-        self._top_label = Label(value='Generated password:')
 
         self._password_text = Label(' ')
         self._password_text.layout.margin = '0 0 0 20px'
@@ -44,24 +46,26 @@ class PassGenGUI(Box):
         Note that the linking to the model is handled here so that the calling
         code, __init__ does not need to know about the details of this widget.
         """
-        explanation = Label('Select one of the groups of special characters '
-                            'below. From 1 to 3 special characters will be included.')
+        explanation1 = Label('Select one of the groups of special characters '
+                             'below.')
+        explanation2 = Label('From 1 to 3 special characters will be included.')
         special = ToggleButtons(options=SPECIAL_GROUPS)
         special.layout.margin = '0 0 0 20px'
         link((special, 'value'),
              (self.model, 'special_character_groups'))
 
         container = VBox(description='Special character',
-                         children=[explanation, special])
+                         children=[explanation1, explanation2, special])
+        container.layout.align_items = 'flex-start'
         return container
 
     def _number_box(self):
         """
         See comments for the special character box above.
         """
-        explanation = Label('Check box to include 1 to 3 numbers '
+        explanation = Label('Check to include 1 to 3 numbers '
                             'in the password:')
-        check = Checkbox()
+        check = Checkbox(style={'description_width': '0px'})
         link((check, 'value'), (self.model, 'include_numbers'))
         container = HBox(description='Number checkbox',
                          children=[explanation, check])
